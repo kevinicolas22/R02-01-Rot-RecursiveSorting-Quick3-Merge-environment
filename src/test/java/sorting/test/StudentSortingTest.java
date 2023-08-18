6015f92f80005409204e3d2ac13f7e61eed5968f
Kevin Nicolas <kevinicolas2511@gmail.com>
2023-08-18 10:46:19 -0300

    .

--- src/test/java/sorting/test/StudentSortingTest.java
+++ src/test/java/sorting/test/StudentSortingTest.java
@@ -37,9 +37,9 @@ public class StudentSortingTest {
 	 */
 	private void getImplementation() {
 		// TODO O aluno deve instanciar sua implementação abaixo ao invés de
-		// null
-		this.implementation = null;
-		Assert.fail("Implementation not provided");
+		// deixar this.implementation = null; apos o termino do lab
+		this.implementation = null; //substituir por this.implementation = new nomeDaClasse()<>;
+		//Assert.fail("Implementation not provided");
 	}
 
 	public void populaVetorTamanhoPar(Integer[] arrayPadrao) {
